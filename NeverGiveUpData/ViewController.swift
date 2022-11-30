47aa98db5b3c5eb35e2d4dc9a961ef500be7b257
rominkgo <rominkgo@gmail.com>
2022-11-30 13:49:58 -0500

    Created the base view and name input

--- NeverGiveUpData/ViewController.swift
+++ NeverGiveUpData/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  NeverGiveUpData
-//
-//  Created by Default on 11/30/22.
-//
 
 import UIKit
 
@@ -11,9 +5,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let name = getNameFromUserDefaults() ?? "You"
+        
+        let introSpeech = ("Welcome back, \(name)")
+        
+        introText.text = introSpeech
     }
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var introText: UILabel!
+    @IBOutlet weak var ageText: UILabel!
+    
+    
+    
 
-
+    @IBOutlet weak var saveContinueButton: UIButton!
+    @IBAction func saveContinue(_ sender: Any) {
+        guard let text = nameField.text, !text.isEmpty else {
+            print("Error!")
+            return
+        }
+        saveNameInUserDefaults(text: text)
+        print("Success!")
+    }
+    
+    let userDefaultsNameKey = "realName"
+    
+    
+    
+    
+    private func saveNameInUserDefaults(text: String) {
+        UserDefaults.standard.set(text, forKey: userDefaultsNameKey)
+    }
+    
+    private func getNameFromUserDefaults() -> String? {
+        let name = UserDefaults.standard.string(forKey: userDefaultsNameKey)
+        return name
+    }
 }
 
