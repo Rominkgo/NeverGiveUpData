acd66ee02a93fa57b265e7066994b0a2bcc8616d
rominkgo <rominkgo@gmail.com>
2022-11-30 21:06:21 -0500

    Added Age Label

--- NeverGiveUpData/ViewController.swift
+++ NeverGiveUpData/ViewController.swift
@@ -5,15 +5,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let name = getNameFromUserDefaults() ?? "You"
-        let introSpeech = ("Welcome back, \(name)")
-        introText.text = introSpeech
+        introScreenNameSetup()
     }
+    
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var ageField: UITextField!
     @IBOutlet weak var introText: UILabel!
     @IBOutlet weak var ageText: UILabel!
     
+    @IBOutlet weak var entryResponder: UILabel!
     @IBOutlet weak var saveContinueButton: UIButton!
     @IBAction func saveContinue(_ sender: Any) {
         guard let text = nameField.text, !text.isEmpty else {
@@ -22,9 +22,11 @@ class ViewController: UIViewController {
         }
         saveNameInUserDefaults(text: text)
         print("Success!")
+        entryResponder.text = "Thank you"
     }
     
     let userDefaultsNameKey = "realName"
+    let userDefaultsAgeKey = "realAge"
     
     private func saveNameInUserDefaults(text: String) {
         UserDefaults.standard.set(text, forKey: userDefaultsNameKey)
@@ -33,6 +35,37 @@ class ViewController: UIViewController {
     private func getNameFromUserDefaults() -> String? {
         let name = UserDefaults.standard.string(forKey: userDefaultsNameKey)
         return name
+    }
+    
+    private func saveAgeInUserDefaults(text: Int) {
+        UserDefaults.standard.set(text, forKey: userDefaultsAgeKey)
+    }
+    
+    private func getAgeFromUserDetails() -> Int? {
+        let age = UserDefaults.standard.integer(forKey: userDefaultsAgeKey)
+        return age
+    }
+    
+    func introScreenNameSetup() {
+        introText.text = "Welcome, please sign up"
+        ageText.text = "Everybody is welcome. Even you"
+        
+        let name = getNameFromUserDefaults() ?? "You"
+        let introSpeech = ("Welcome back, \(name)")
+        introText.text = introSpeech
+    }
+    
+    func introScreenAgeSetup(){
+        let age = getAgeFromUserDetails() ?? 0
+        var smackTalk: String
+
+        if age < 26 {
+            let smackTalk = ("Wow! \(age) ? What a baby")
+        } else {
+            let smackTalk = ("Wow you are so old")
+        }
+        
+        ageText.text = smackTalk
     }
 }
 
